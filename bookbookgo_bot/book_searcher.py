60aa05841512cb742d5d4b51b0cec8a972e0815b
Le Wang <lewang.dev@gmail.com>
2023-04-12 18:08:45 +0800

    add start handle

--- bookbookgo_bot/book_searcher.py
+++ bookbookgo_bot/book_searcher.py
@@ -2,11 +2,17 @@ import requests
 
 from config import BOOK_SEARCHER_BASE_URL
 
+
 def search_books(keyword, limit=100):
     url = f"{BOOK_SEARCHER_BASE_URL}/search"
-    query=None
+    query = None
     if type(keyword) is dict:
-        query = " ".join(map(lambda f: f'{f[0]}:{f[1]}', filter(lambda f: False if not f[1] else True, keyword.items())))
+        query = " ".join(
+            map(
+                lambda f: f"{f[0]}:{f[1]}",
+                filter(lambda f: False if not f[1] else True, keyword.items()),
+            )
+        )
     elif type(keyword) is str:
         query = keyword
     else:
@@ -16,14 +22,15 @@ def search_books(keyword, limit=100):
     r = requests.get(url, params)
     return r.json()
 
+
 def sort_books(books):
-    extensions = ['epub', 'mobi', 'azw3', 'pdf', 'txt']
+    extensions = ["epub", "mobi", "azw3", "pdf", "txt"]
     books_group_by_ext = {}
     for ext in extensions:
         books_group_by_ext[ext] = []
 
     for book in books:
-        book_ext = book['extension']
+        book_ext = book["extension"]
         if book_ext not in extensions:
             continue
 
@@ -35,9 +42,10 @@ def sort_books(books):
 
     return books_sorted
 
+
 if __name__ == "__main__":
-    res = search_books('余华')
+    res = search_books("余华")
     print(res)
 
-    res = search_books(dict(title='红楼梦', author='曹雪芹'))
+    res = search_books(dict(title="红楼梦", author="曹雪芹"))
     print(res)
--- bookbookgo_bot/main.py
+++ bookbookgo_bot/main.py
@@ -1,150 +1,134 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument, wrong-import-position
-
+import os
 import logging
+import asyncio
 import traceback
+import html
+import json
+import tempfile
+from pathlib import Path
+from datetime import datetime
 
-from telegram import __version__ as TG_VER
-
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
-
-if __version_info__ < (20, 0, 0, "alpha", 5):
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.constants import ParseMode
+import telegram
+from telegram import (
+    Update,
+    User,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    BotCommand,
+)
 from telegram.ext import (
     Application,
+    ApplicationBuilder,
+    CallbackContext,
     CommandHandler,
-    ContextTypes,
-    ConversationHandler,
     MessageHandler,
-    PicklePersistence,
+    CallbackQueryHandler,
+    AIORateLimiter,
     filters,
 )
-from config import BOT_TOKEN, IPFS_GATEWAY_BASE_URL, LOG_LEVEL
-from book_searcher import search_books, sort_books
+from telegram.constants import ParseMode, ChatAction
+
+import config
+
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOG_LEVEL
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=config.LOG_LEVEL,
 )
 logger = logging.getLogger(__name__)
 
-TITLE, AUTHOR = range(2)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text(
-        "Please input the title of the book\n\n"
-        "Send /cancel to stop this session."
+HELP_MESSAGE = """Commands:
+⚪ /start – Start to search books
+⚪ /ai – Tell AI what books you want to read
+⚪ /help – Show help
+"""
+
+
+def split_text_into_chunks(text, chunk_size):
+    for i in range(0, len(text), chunk_size):
+        yield text[i : i + chunk_size]
+
+
+async def start_handle(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+
+    reply_text = "Hi! I'm <b>BookBookBot</b>\n\n"
+    reply_text += HELP_MESSAGE
+    reply_text += "\nAnd now... try to search books:\n"
+    reply_text += "红楼梦\n"
+    reply_text += "红楼梦 曹雪芹\n"
+    reply_text += "红楼梦 曹雪芹 epub\n"
+
+    await update.message.reply_text(reply_text, parse_mode=ParseMode.HTML)
+
+
+async def help_handle(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    await update.message.reply_text(HELP_MESSAGE, parse_mode=ParseMode.HTML)
+
+
+async def error_handle(update: Update, context: CallbackContext) -> None:
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    try:
+        # collect error message
+        tb_list = traceback.format_exception(
+            None, context.error, context.error.__traceback__
+        )
+        tb_string = "".join(tb_list)
+        update_str = update.to_dict() if isinstance(update, Update) else str(update)
+        message = (
+            f"An exception was raised while handling an update\n"
+            f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+            "</pre>\n\n"
+            f"<pre>{html.escape(tb_string)}</pre>"
+        )
+
+        # split text into multiple messages due to 4096 character limit
+        for message_chunk in split_text_into_chunks(message, 4096):
+            try:
+                await context.bot.send_message(
+                    update.effective_chat.id, message_chunk, parse_mode=ParseMode.HTML
+                )
+            except telegram.error.BadRequest:
+                # answer has invalid characters, so we send it without parse_mode
+                await context.bot.send_message(update.effective_chat.id, message_chunk)
+    except:
+        await context.bot.send_message(
+            update.effective_chat.id, "Some error in error handler"
+        )
+
+
+async def post_init(application: Application):
+    await application.bot.set_my_commands(
+        [
+            BotCommand("/start", "Start to search books"),
+            BotCommand("/ai", "Tell AI what books you want to read"),
+            BotCommand("/help", "Show help message"),
+        ]
     )
 
-    return TITLE
 
-
-async def title(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    user = update.message.from_user
-    logger.info("User %s's input: %s", user.first_name, update.message.text)
-    title = update.message.text
-    r = search_books(dict(title=title))
-    books = r['books']
-    books_sorted = sort_books(books)
-    context.user_data['books'] = books_sorted
-    context.user_data['title'] = title
-
-    if len(books_sorted) == 0:
-        text = f'No book found with title: {title}, try another title below.'
-        await update.message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-        return TITLE
-    else:
-        book = books_sorted[0]
-        title = book['title']
-        author = book['author']
-        extension = book['extension']
-        ipfs_cid = book['ipfs_cid']
-        url_prefix = f"{IPFS_GATEWAY_BASE_URL}/ipfs/"
-        url = f"{url_prefix}{ipfs_cid}?filename={title}.{extension}"
-        text = f"Title: {title} Author: {author} Extension: {extension} URL: <a href=\"{url}\">Download</a>\n\n" \
-        "If the book above isn't your want, specify the author's name below\n\n" \
-        "Send /cancel to stop this session then /start to search another title." 
-
-        await update.message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-        return AUTHOR
-
-async def author(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    author = update.message.text
-    books_saved = context.user_data['books']
-    title = context.user_data['title']
-    books = list(filter(lambda n: True if author in n['author'] else False, books_saved))
-    if len(books) == 0:
-        text = f'No book found with title: {title} and author: {author}.\n\n' \
-                "Continue to try another author's name.\n\n" \
-                "Or send /cancel to stop this session then /start to search another title." 
-    else:
-        book = books[0]
-        title = book['title']
-        author = book['author']
-        extension = book['extension']
-        ipfs_cid = book['ipfs_cid']
-        url_prefix = f"{IPFS_GATEWAY_BASE_URL}/ipfs/"
-        url = f"{url_prefix}{ipfs_cid}?filename={title}.{extension}"
-        text = f"Title: {title} Author: {author} Extension: {extension} URL: <a href=\"{url}\">Download</a>\n\n" \
-        "If the book above isn't your want, specify the author's name below\n\n" \
-        "Send /cancel to stop this session then /start to search another title." 
-
-    await update.message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-    return AUTHOR
-
-
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! Or /start another search"
+def run_bot() -> None:
+    application = (
+        ApplicationBuilder()
+        .token(config.BOT_TOKEN)
+        .concurrent_updates(True)
+        .rate_limiter(AIORateLimiter(max_retries=5))
+        .post_init(post_init)
+        .build()
     )
 
-    return ConversationHandler.END
+    # add handlers
+    application.add_handler(CommandHandler("start", start_handle))
+    application.add_handler(CommandHandler("help", help_handle))
+    application.add_error_handler(error_handle)
 
-
-async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = "".join(tb_list)
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    logger.error(f"Exception while handling an update: {tb_string}, {update_str}")
-
-
-def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    persistence = PicklePersistence(filepath="bookbookgo_bot.pickle")
-    application = Application.builder().token(BOT_TOKEN).persistence(persistence).build()
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, title)],
-            AUTHOR: [MessageHandler(filters.TEXT & ~filters.COMMAND, author)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-        name="my_conversation",
-        persistent=True,
-    )
-
-    application.add_handler(conv_handler)
-
-    # Add the error handler
-    application.add_error_handler(error_handler)
-
-    # Run the bot until the user presses Ctrl-C
+    # start the bot
     application.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    run_bot()
